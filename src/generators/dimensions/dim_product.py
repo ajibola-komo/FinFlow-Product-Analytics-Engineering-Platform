54f18a8cf448669c1baa65b22ea33cca968134d5
ajibola-komo <ajibolaokomolafe@gmail.com>
2026-06-04 00:55:30 +0100

    updated ddl definitions

--- src/generators/dimensions/dim_product.py
+++ src/generators/dimensions/dim_product.py
@@ -1,20 +1,18 @@
-from src.config.paths import(DDL_DIM_PRODUCT_PATH, PRODUCTS_PARQUET_PATH, RAW_DIM_PRODUCT_PATH)
+import pandas as pd
+import numpy as np
+from src.config.paths import (DDL_DIM_PRODUCT_PATH, RAW_DIM_PRODUCT_PATH, PRODUCTS_PARQUET_PATH)
 
 def generate_products(conn):
-    #read SQL file for product DDL and execute it to create the dim_product table
+
     create_db = DDL_DIM_PRODUCT_PATH.read_text()
 
     conn.execute(create_db)
 
-    #read the dim_product csv file and export it to a parquet file
     conn.execute(f'''
-
-        INSERT INTO dim_product SELECT * FROM read_csv_auto('{RAW_DIM_PRODUCT_PATH}')
-
+            INSERT INTO DIM_PRODUCT SELECT * FROM read_csv_auto('{RAW_DIM_PRODUCT_PATH}')
     ''')
 
-    #write the dim_product table to a parquet file
-    conn.execute(f"""
-                    COPY dim_product TO '{PRODUCTS_PARQUET_PATH}' (FORMAT PARQUET, OVERWRITE_OR_IGNORE TRUE) 
-                """)
-    
+    conn.execute(f'''
+                COPY DIM_PRODUCT TO '{PRODUCTS_PARQUET_PATH}' (FORMAT PARQUET, OVERWRITE_OR_IGNORE TRUE) 
+
+''')

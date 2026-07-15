4c32ebcc3e2a096591f89e02910bbb4784c34e28
ajibola-komo <ajibolaokomolafe@gmail.com>
2026-07-15 14:42:41 +0100

    updated the source data dictionary

--- sql/ddl/facts/fact_wallet_balance.sql
+++ sql/ddl/facts/fact_wallet_balance.sql
@@ -3,10 +3,10 @@ create table fact_wallet_balance(
     user_id int not null unique,
     current_balance decimal(15,2) not null default 0.00,
     updated_at timestamp not null,
-    last_transaction_id int,
-    last_event_id int,
+    updated_date_id int,
+    last_transaction_id int, -- degenerate key
+    last_event_id int, -- degenerate key
     foreign key(wallet_id) references dim_wallet(wallet_id),
     foreign key(user_id) references dim_user(user_id),
-    foreign key(last_transaction_id) references fact_transaction(transaction_id),
-    foreign key(last_event_id) references fact_user_event(event_id)
+    foreign key(updated_date_id) references dim_date(date_id)
 );

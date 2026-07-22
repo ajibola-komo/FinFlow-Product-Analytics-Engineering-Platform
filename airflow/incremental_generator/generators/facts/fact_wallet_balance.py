2a25d7cd1c812f217ccde311f4f54b151b43b134
ajibola-komo <ajibolaokomolafe@gmail.com>
2026-07-22 22:02:52 +0100

    updated fact_wallet_balance incremental model

--- airflow/incremental_generator/generators/facts/fact_wallet_balance.py
+++ airflow/incremental_generator/generators/facts/fact_wallet_balance.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
 
-from src.config.paths import (FACT_WALLET_BALANCE_PARQUET_PATH, DDL_FACT_WALLET_BALANCE_PATH)
+from incremental_generator.config.constants import (GENERATION_START_DATE, GENERATION_START_TIMESTAMP, GENERATION_END_TIMESTAMP)
+
 
 def generate_wallet_balance(conn):
 
-    create_db = DDL_FACT_WALLET_BALANCE_PATH.read_text()
-
-    conn.execute(create_db)
-
-    wallets_data = conn.execute(f'''select wallet_id, user_id, wallet_created_at from dim_wallet order by wallet_id''').df()
+    wallets_data = conn.execute(f'''
+            SELECT wallet_id, user_id, wallet_created_at from dim_wallet where wallet_created_at between '{GENERATION_START_TIMESTAMP}'
+            AND '{GENERATION_END_TIMESTAMP}'
+    ''').df()
 
     wallet_ids = wallets_data['wallet_id']
     user_ids = wallets_data['user_id']
@@ -37,7 +37,5 @@ def generate_wallet_balance(conn):
                      select wallet_id, user_id, current_balance, updated_at, last_transaction_id, last_event_id, created_at, last_updated_at from wallet_balance_df''')
     
 
-    conn.execute(f''' COPY (select * from fact_wallet_balance) to '{FACT_WALLET_BALANCE_PARQUET_PATH}' (FORMAT PARQUET)''')
-
     conn.unregister('wallet_balance_df')
-
+    
